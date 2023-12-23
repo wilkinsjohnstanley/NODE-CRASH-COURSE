9aec819330b1d6aa0650a9cafaea3fcdffd9cf0b
John Wilkins <wilkinsjohnstanley@gmail.com>
2023-12-23 17:43:48 -0600

    Interval and Timeout fun!

--- test.js
+++ test.js
@@ -6,9 +6,18 @@ CTRL + / = comment out a highlighted code block
 
 */
 
-const greet = (name) => {
-    console.log(`Hello, ${name}`)
-}
+// const greet = (name) => {
+//     console.log(`Hello, ${name}`)
+// }
 
-greet('mario');
-greet('yoshi');
+// greet('mario');
+// greet('yoshi');
+
+setTimeout(()=>{
+    console.log('in the timeout');
+    clearInterval(int);
+}, 3000);
+
+const int = setInterval(()=>{
+    console.log('in the interval');
+}, 1000);
