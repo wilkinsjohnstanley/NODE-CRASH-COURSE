2f192055aea10afd8e252c5056963bcac21800e1
John Wilkins <wilkinsjohnstanley@gmail.com>
2023-12-23 16:11:33 -0600

    first commit

---
+++ test.js
@@ -0,0 +1,14 @@
+// const name = 'yoshi';
+// console.log(name);
+
+/*
+CTRL + / = comment out a highlighted code block
+
+*/
+
+const greet = (name) => {
+    console.log(`Hello, ${name}`)
+}
+
+greet('mario');
+greet('yoshi');
